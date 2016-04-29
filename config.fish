1a03c50fba26bf53ebea9aad3a2fedd7d391c647
Nick Jones <nick.jones@datacentred.co.uk>
2016-04-29 17:11:49 +0100

    Initial Fish commit

---
+++ config.fish
@@ -0,0 +1,44 @@
+# User customisation for the Fish shell
+
+# vi related
+fish_vi_mode
+function fish_mode_prompt; end
+
+# better compatibility for fzf with tmux
+set -U FZF_TMUX 1
+
+# some other {sensible,useful} shortcuts
+alias pg='ps auwwx | grep -i -e ^USER -e '
+alias publicip='curl http://ifconfig.me'
+alias webserver="ifconfig | grep 'inet ' | grep -v 127.0.0.1; python -m SimpleHTTPServer"
+alias cls='clear'
+alias ls='ls -FG'
+alias tma='tmux attach-session -t'
+alias view='vim -R'
+alias se='sudoedit'
+alias topmem='ps -eo pmem,pcpu,rss,vsize,args | sort -k 1 -r | less'
+alias rip='dvdbackup -i /dev/sr0 -o . -M'
+# alias rake="noglob rake"
+alias trillian='mosh trillian.dischord.org'
+alias zarquon='mosh zarquon.dischord.org'
+alias sshx='ssh -c arcfour,blowfish-cbc -XC'
+alias pwplz='apg -n 1 -m 12 -x 12 -M NC'
+alias keyplz='openssl rand -hex 10'
+alias md='open -a Marked.app'
+alias uuidgen="uuidgen | tr 'A-Z' 'a-z'"
+
+# <3 vagrant
+alias vup='vagrant up --no-install-provider'
+alias vprov='vagrant provision'
+alias vstat='vagrant status'
+alias vhalt='vagrant halt'
+alias vnuke='vagrant destroy -f'
+alias vssh='vagrant ssh'
+alias vhosts='vagrant hostmanager --provider=vmware_fusion'
+
+# git stuff
+alias gitl='git log --pretty=format:"%h - %an, %ar : %s"'
+alias gits='git shortlog --numbered --summary'
+alias gitrs="git reset --soft 'HEAD^'"
+alias gitrsh='git reset --hard HEAD'
+alias gitsup='git submodule sync ; git submodule update --init'
