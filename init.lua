a5c444627499904cbd16ee0c77b616369768f413
Nick Jones <nick@dischord.org>
2024-03-22 10:58:45 +0000

    [neovim] Add LSP and completion plugins plus options

--- init.lua
+++ init.lua
@@ -75,6 +75,284 @@ require("lazy").setup({
         require("telescope").load_extension("ui-select")
       end,
     },
+
+  -- Language server (LSP)
+  {
+    "neovim/nvim-lspconfig", 
+    config = function ()
+      util = require "lspconfig/util"
+
+      local capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      require("lspconfig").gopls.setup({
+        capabilities = capabilities,
+        flags = { debounce_text_changes = 200 },
+        settings = {
+          gopls = {
+            usePlaceholders = true,
+            gofumpt = true,
+            analyses = {
+              nilness = true,
+              unusedparams = true,
+              unusedwrite = true,
+              useany = true,
+            },
+            codelenses = {
+              gc_details = false,
+              generate = true,
+              regenerate_cgo = true,
+              run_govulncheck = true,
+              test = true,
+              tidy = true,
+              upgrade_dependency = true,
+              vendor = true,
+            },
+            experimentalPostfixCompletions = true,
+            completeUnimported = true,
+            staticcheck = true,
+            directoryFilters = { "-.git", "-node_modules" },
+            semanticTokens = true,
+            hints = {
+              assignVariableTypes = true,
+              compositeLiteralFields = true,
+              compositeLiteralTypes = true,
+              constantValues = true,
+              functionTypeParameters = true,
+              parameterNames = true,
+              rangeVariableTypes = true,
+            },
+          },
+        },
+      })
+
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      require("lspconfig").terraformls.setup({
+        on_attach = on_attach,
+        flags = { debounce_text_changes = 150 },
+        capabilities = capabilities,	
+      })
+
+      require("lspconfig").ansiblels.setup{}
+
+    end,
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+      "onsails/lspkind-nvim",
+    },
+    config = function()
+      local cmp = require("cmp")
+      local luasnip = require("luasnip")
+      local lspkind = require("lspkind")
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+      luasnip.config.setup {}
+
+      local has_words_before = function()
+        unpack = unpack or table.unpack
+        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+      end
+
+      require('cmp').setup({
+        snippet = {
+            expand = function(args)
+              luasnip.lsp_expand(args.body)
+            end,
+        },
+        formatting = {
+          format = lspkind.cmp_format {
+            with_text = true,
+            menu = {
+              buffer = "[Buffer]",
+              nvim_lsp = "[LSP]",
+              nvim_lua = "[Lua]",
+            },
+          },
+        },
+        mapping = cmp.mapping.preset.insert {
+          ['<C-n>'] = cmp.mapping.select_next_item(),
+          ['<C-p>'] = cmp.mapping.select_prev_item(),
+          ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+          ['<CR>'] = cmp.mapping.confirm { select = true },
+          ['<Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif luasnip.expand_or_locally_jumpable() then 
+              luasnip.expand_or_jump()
+            elseif has_words_before() then
+              cmp.complete()
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+          ['<S-Tab>'] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+              luasnip.jump(-1)
+            else
+              fallback()
+            end
+          end, { 'i', 's' }),
+        },
+        -- don't auto select item
+        preselect = cmp.PreselectMode.None,
+        window = {
+          documentation = cmp.config.window.bordered(),
+        },
+        view = {
+          entries = {
+            name = "custom",
+            selection_order = "near_cursor",
+          },
+        },
+        confirm_opts = {
+          behavior = cmp.ConfirmBehavior.Insert,
+        },
+        sources = {
+          { name = 'nvim_lsp' },
+          { name = "luasnip", keyword_length = 2},
+          { name = "buffer", keyword_length = 5},
+        },
+      })
+    end,
+  },
+
+  --- Highlight, edit, and navigate code
+  {
+    "windwp/nvim-autopairs",
+    config = function() 
+      require("nvim-autopairs").setup {
+        check_ts = true,
+      }
+    end
+  },
+  { 
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter/nvim-treesitter-textobjects',
+    },
+    build = ":TSUpdate",
+    config = function()
+      require('nvim-treesitter.configs').setup({
+        ensure_installed = {
+          'go',
+          'gomod',
+          'proto',
+          'lua',
+          'ruby',
+          'vimdoc',
+          'vim',
+          'bash',
+          'fish',
+	  'hcl',
+	  'python',
+          'json',
+          'markdown',
+          'markdown_inline',
+          'mermaid',
+        },
+        indent = { enable = true },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "<space>", -- maps in normal mode to init the node/scope selection with space
+            node_incremental = "<space>", -- increment to the upper named parent
+            node_decremental = "<bs>", -- decrement to the previous node
+            scope_incremental = "<tab>", -- increment to the upper scope (as defined in locals.scm)
+          },
+        },
+        autopairs = {
+          enable = true,
+        },
+        highlight = {
+          enable = true,
+
+          -- Disable slow treesitter highlight for large files
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+
+          -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+          -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+          -- Using this option may slow down your editor, and you may see some duplicate highlights.
+          -- Instead of true it can also be a list of languages
+          additional_vim_regex_highlighting = false,
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['aa'] = '@parameter.outer',
+              ['ia'] = '@parameter.inner',
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+              ["iB"] = "@block.inner",
+              ["aB"] = "@block.outer",
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']]'] = '@function.outer',
+            },
+            goto_next_end = {
+              [']['] = '@function.outer',
+            },
+            goto_previous_start = {
+              ['[['] = '@function.outer',
+            },
+            goto_previous_end = {
+              ['[]'] = '@function.outer',
+            },
+          },
+          swap = {
+            enable = true,
+            swap_next = {
+              ['<leader>sn'] = '@parameter.inner',
+            },
+            swap_previous = {
+              ['<leader>sp'] = '@parameter.inner',
+            },
+          },
+        },
+      })
+    end,
+  },
+
+
+})
+
+--- Filetypes
+
+vim.filetype.add({
+  extension = {
+    yml = 'yaml.ansible',
+    hcl = 'terraform'
+  }
 })
 
 --- Keymappings
@@ -106,4 +384,6 @@ vim.opt.mouse = ""
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
 vim.opt.modelines = 5
+
+
 
